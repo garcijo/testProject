a376e5e2aaf253f1aece26227415afb820b2e92e
garcijo <garcijo94@gmail.com>
2016-07-22 13:34:27 -0400

    Login Screen
    Added login and registration page

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,9 @@
 <?php
 // Routes
+$app->get('/login', function ($request, $response, $args) {
+    // Render index view
+    return $this->renderer->render($response, 'login.phtml', $args);
+});
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
@@ -8,3 +12,5 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+
